a3967ecf5cb390a72103dd9ceebc879a96617352
aleleotta <aleleotta@106-02.info2>
2022-10-31 10:21:56 +0100

    Exercise 3 fixed!

--- While Exercises Topic 3/src/exercises/Exercise3_1.java
+++ While Exercises Topic 3/src/exercises/Exercise3_1.java
@@ -10,17 +10,14 @@ public class Exercise3_1 {
 		Scanner sc = new Scanner(System.in); //Scanner declared.
 			System.out.println("Please introduce a positive number:");  //Program starts asking number to the user
 			num = sc.nextInt(); //User input
-			sum = num; //First number goes into sum
 			while (num >= 0) { //Iteration loop
+				sum = sum + num; //This time the current sum will be added to the new sum with the new number.
+				System.out.println("Sum: " + sum); //New sum gets printed.
+				increment++;
 				System.out.println("Please introduce a positive number:"); //Same as before while block.
 				num = sc.nextInt();
-				if (num >= 0) {
-					sum = sum + num; //This time the current sum will be added to the new sum with the new number.
-					System.out.println("Sum: " + sum); //New sum gets printed.
-					increment++;
-					average = sum / increment; //Same as before while block.
-				}
 			}
+		average = sum / increment; //Always goes after while block.
 		System.out.println("Here is the average from the sum (" + sum + ") of all numbers you have introduced: " + average); //Final output of sum before terminating the program.
 		sc.close(); //Scanner closes
 	}
