527436b384988349084cb1d829b7873988e58927
aleleotta <aleleotta@106-02.info2>
2022-11-03 14:41:17 +0100

    Exercise 7 and 8 finished!

--- While Exercises Topic 3/src/exercises/Exercise7.java
+++ While Exercises Topic 3/src/exercises/Exercise7.java
@@ -3,16 +3,28 @@ import java.util.*;
 
 public class Exercise7 {
 	public static void main(String[] args) {
-		double secretN;
+		int secretN;
 		int userN = 0;
 		Scanner sc = new Scanner(System.in);
-		secretN = Math.random();
-		secretN = Math.round(secretN);
+		secretN = (int) (Math.random() * 100); //Given functionality to the secret number, casting int type, (Math.random() + range)
 		System.out.println(secretN);
-		while (secretN != userN) {
-			
+		System.out.println("Try to guess the secret number. \nType in a random number from 0 to 100");
+		while (secretN != userN && userN >= 0) {
+			if (userN > secretN) {
+				System.out.println("Your number is greater than the secret number.");
+			}
+			if (userN < secretN) {
+				System.out.println("Your number is less than the secret number.");
+			}
+			userN = sc.nextInt();
 		}
+		if (userN >= 0) {	
+			System.out.println("Congrats!!! You guessed the number!");
+			
+		} else {
+			System.out.println("The secret number was: " + secretN + " You lost! Try again next time.");
+		}
+		System.out.println("Game Over");
 		sc.close();
 	}
 }
-//Usar Math.random()
--- While Exercises Topic 3/src/exercises/Exercise8.java
+++ While Exercises Topic 3/src/exercises/Exercise8.java
@@ -3,7 +3,25 @@ import java.util.*;
 
 public class Exercise8 {
 	public static void main(String[] args) {
+		int maxHeight = 0;
+		int height = 0;
+		int trees = 0;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce a height: ");
+		System.out.println("*Introduce a negative number to terminate.*");
+		height = sc.nextInt();
+		while (height > 0) {
+			trees++;
+			if (height > maxHeight) {
+				maxHeight =+ height;
+			}
+			System.out.println("Number of trees: " + trees);
+			System.out.println("Maximum height: " + maxHeight);
+			height = sc.nextInt();
+		}
+		System.out.println("The highest tree is " + maxHeight + " cm high.");
+		System.out.println("The number of trees in the area is: " + trees);
+		System.out.println("Have a nice day!");
 		sc.close();
 	}
 }
